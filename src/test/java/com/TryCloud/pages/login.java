f354ccb927c0474b29b2a8afc2f4c2549110e6bb
konusulanalmanca@gmail.com <canismailffm@gmail.com>
2020-04-10 11:15:17 +0200

    FirstGitCommit

---
+++ src/test/java/com/TryCloud/pages/login.java
@@ -0,0 +1,12 @@
+package com.TryCloud.pages;
+
+import sun.jvm.hotspot.HelloWorld;
+
+public class login {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWelt");
+    }
+
+}
+
